8c847700c86507851a7e3d85dd09084eb7fca4a3
legobatkata <legobatkata@abv.bg>
2022-11-28 16:42:20 +0200

    implemented some tasks, changed the target for tests from "testing" to "testingTarget" because some naming issue

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,15 +1,20 @@
 #include "solution.h"
+#include <iostream>
 
 int countElements(Node* tree) {
-    return -1;
+    if(tree == nullptr) return 0;
+    return countElements(tree->left) + countElements(tree->right) + 1;
 }
 
 int countLeafs(Node* tree) {
-    return -1;
+    if(tree == nullptr) return 0;
+    if(tree->left == nullptr && tree->right == nullptr) return 1;
+    return countLeafs(tree->left) + countLeafs(tree->right);
 }
 
 int height(Node* tree) {
-    return -1;
+    if(tree == nullptr) return 0;
+    return std::max(height(tree->left), height(tree->right)) + 1;
 }
 
 int sumElements(Node* tree) {
@@ -24,6 +29,19 @@ bool isSymmetric(Node* tree) {
     return false;
 }
 
+bool checkLeftRight(Node* tree){
+    if(tree->left == nullptr && tree->right == nullptr) return true;
+    if(tree->left == nullptr && tree->right != nullptr){
+        return tree->key <= tree->right->key;
+    }
+    if(tree->left != nullptr && tree->right == nullptr){
+        return tree->left->key < tree->key;
+    }
+    if(tree->left != nullptr && tree->right != nullptr){
+        return (tree->left->key < tree->key) && (tree->key <= tree->right->key);
+    }
+}
 bool isBST(Node* tree) {
-    return false;
+    if(tree == nullptr) return true;
+    return checkLeftRight(tree) && isBST(tree->left) && isBST(tree->right);
 }
